ec05c2561e2f2a949edb8cdbf240d03418a7739e
IOneL1R <i8266434@gmail.com>
2025-07-09 18:59:31 +0300

    Сделали регистрацию

--- users/views.py
+++ users/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, redirect
 from .models import Profile
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
-
-
+@login_required
 def profile(request):
-    user = User.objects.filter(is_superuser=True).first()
-    profile = Profile.objects.get(user = user)
+    user = request.user
+    profile = user.profile
     return render(request,"users/profile.html",{"profile":profile,"user":user})
 
 def auth1(request):
@@ -19,7 +19,6 @@ def auth1(request):
     else:
         form = UserCreationForm()
     return render(request,"users/register.html",{"form":form})
-
 
 
 
